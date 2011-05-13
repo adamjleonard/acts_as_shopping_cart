bbc9239ce2765221e43cc9d31cb8b5e48cd05a94
Adam Leonard <adamjamesleonard@gmail.com>
2011-05-13 11:21:17 -0400

    Updates

--- lib/active_record/acts/shopping_cart/cart_instance_methods.rb
+++ lib/active_record/acts/shopping_cart/cart_instance_methods.rb
@@ -2,29 +2,37 @@ module ActiveRecord
   module Acts
     module ShoppingCart
       module InstanceMethods
-        
+
         #
         # Returns the subtotal by summing the price times quantity for all the items in the cart
         #
         def subtotal
           ("%.2f" % cart_items.sum("price * quantity")).to_f
         end
-        
+
         #
         # Returns the total by summing the subtotal, taxes and shipping_cost
         #
         def total
-          ("%.2f" % (subtotal + self.taxes + shipping_cost)).to_f
+          total = subtotal
+          total += taxes         if self.respond_to?(:taxes)
+          total += shipping_cost if self.respond_to?(:shipping_cost)
+
+          ("%.2f" % total).to_f
         end
 
         #
         # Adds a product to the cart
         #
-        def add(object, price, quantity = 1)
+        def add(object, price, quantity = 1, item = nil)
           cart_item = item_for(object)
 
           unless cart_item
-            cart_items.create(:item => object, :price => price, :quantity => quantity)
+            unless item.nil?
+              cart_items.create(:item => item, :item_type => item.class.name, :price => price, :quantity => quantity)
+            else
+              cart_items.create(:item => object, :price => price, :quantity => quantity)
+            end
           else
             cart_item.quantity = (cart_item.quantity + quantity)
             cart_item.save
@@ -35,22 +43,22 @@ module ActiveRecord
         # Delete an item from the cart
         #
         def delete(object)
-          cart_item = item_for(object)
-          cart_items.delete(cart_item)
+          cart_items.destroy(item_for(object))
         end
 
         #
         # Remove an item from the cart
         #
         def remove(object, quantity = 1)
-          cart_item = item_for(object)
-          if cart_item
+          if cart_item = item_for(object)
             if cart_item.quantity <= quantity
               cart_items.delete(cart_item)
             else
               cart_item.quantity = (cart_item.quantity - quantity)
               cart_item.save
             end
+          else
+            false
           end
         end
 
@@ -59,6 +67,13 @@ module ActiveRecord
         #
         def total_unique_items
           cart_items.sum(:quantity)
+        end
+
+        #
+        # Cart items enumerable
+        #
+        def items
+          cart_items
         end
       end
     end
